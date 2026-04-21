9bcf81dc4322fb4ddb2f99bb6f2d23990b093b37
garyb128 <gbrankin@gmx.co.uk>
2026-04-21 16:11:39 +0100

    Gutted weapon system and started again

---
+++ Assets/_Project/Code/Weapons/WeaponViewDefinition.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapons/Weapon View Definition")]
+public class WeaponViewDefinition : MonoBehaviour
+{
+    public GameObject modelPrefab;
+    public RuntimeAnimatorController animatorController;
+}
